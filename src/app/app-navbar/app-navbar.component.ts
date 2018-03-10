f93ab32764f829fc0859a53a798653dc48968d74
Nate Hascup <nshascup@gmail.com>
2018-03-10 13:44:34 -0800

    Navbar uses templating of projects in DB for project list dropdown. Routing kiiiinda works

--- src/app/app-navbar/app-navbar.component.ts
+++ src/app/app-navbar/app-navbar.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AngularFireDatabase} from "angularfire2/database";
+import {Observable} from "rxjs/Observable";
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './app-navbar.component.html',
   styleUrls: ['./app-navbar.component.css']
 })
 export class AppNavbarComponent implements OnInit {
+  projectsObservable: Observable<any[]>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.projectsObservable = this.getProjects('/projects');
+  }
+
+  getProjects(listPath): Observable<any[]> {
+    return this.db.list(listPath).valueChanges();
   }
 
 }
+
